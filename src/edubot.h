4143444f3a68699accb8d7ff6297778c6333bb82
Alexander Stephan <xan@discolovers.de>
2019-04-05 19:00:14 +0200

    Added missing declarations

--- src/edubot.h
+++ src/edubot.h
@@ -1,30 +1,46 @@
 #ifndef EDUBOT_EDUBOT_H
 #define EDUBOT_EDUBOT_H
 
+/* Motor pins */
+#define MOTOR_A_SPEED 13  // enA
+#define MOTOR_A_ENABLE1 2 // in1
+#define MOTOR_A_ENABLE2 0  // in2
+
+#define MOTOR_B_SPEED 12  // enB
+#define MOTOR_B_ENABLE1 5  // in3                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+#define MOTOR_B_ENABLE2 4  // in42
+ 
+/* Servo pin */
+#define SERVO_PWM 4
+
+/* Ultrasonic sensor pins */
+#define ECHO_PIN 13
+#define TRIG_PIN 12
+
 typedef enum {LEFT, RIGHT} direction_t;
 
-/****Ultrasonic Sensor****/
+/* Ultrasonic sensor */
 void getDistance();
 
-/****Robot Movement****/
+/* Robot movement */
+void setDefaultSpeed();
+void initServo();
 void driveForward();
+void driveBackward();
 void changeDirA();
 void changeDirB();
-void driveBackward();
 void stopWheel(bool left);
 void stopAll();
-void turnDegree();
-void turnTime();
+void turnDir(direction_t dir, int time);
 void turnLeft();
 void turnRight();
 void turnServo();
 void collisionHandling();
 void driveSpiral();
 
-/****Server****/
+/* Server */
 String prepareHtmlPage();
 void handleGet();
 void handleNotFound();
-
 
 #endif //EDUBOT_EDUBOT_H
