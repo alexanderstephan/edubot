4cbca5921048f87f643a47694fa1d1ed7033b136
Alexander Stephan <xan@discolovers.de>
2019-04-23 13:46:44 +0200

    Added remote uploading via OTA

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <FS.h>
+#include <ArduinoOTA.h>
 #include "edubot.h"
 
 #define DEFAULT_SPEED 512
@@ -315,6 +316,41 @@ void setup() {
         Serial.print("IP address: ");
         Serial.println(WiFi.softAPIP());
     }
+
+
+    ArduinoOTA.setHostname("ESP8266");
+    ArduinoOTA.setPassword("esp8266");
+
+    if (debugLevel > 1) {
+        ArduinoOTA.onStart([]() {
+            Serial.println("Start");
+        });
+
+        ArduinoOTA.onEnd([]() {
+            Serial.println("\nEnd");
+        });
+
+        ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+
+            Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+        });
+
+        ArduinoOTA.onError([](ota_error_t error) {
+                Serial.printf("Error[%u]: ", error);
+                if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+                else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+                else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+                else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+                else if (error == OTA_END_ERROR) Serial.println("End Failed");
+        });
+    }
+    
+    ArduinoOTA.begin();
+
+    if (debugLevel > 1) {
+    Serial.println("OTA ready");
+    }
+
     // Handle server requests
     server.on("/",HTTP_GET,handleGet);
 
@@ -366,7 +402,9 @@ void setup() {
     }
 }
 
-void loop() { 
+void loop() {
+    // Handle remote uploading
+    ArduinoOTA.handle();
     // Handle server
     server.handleClient();
     // Force different states for continous execution
